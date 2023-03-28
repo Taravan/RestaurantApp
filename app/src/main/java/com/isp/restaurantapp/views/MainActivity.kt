8163280b15d28278444f4e7275e8599f31a68fba
Tomáš Macák <macaktom@seznam.cz>
2023-03-28 17:52:05 +0200

    Basic qr reader done

--- app/src/main/java/com/isp/restaurantapp/views/MainActivity.kt
+++ app/src/main/java/com/isp/restaurantapp/views/MainActivity.kt
@@ -1,21 +1,183 @@
 package com.isp.restaurantapp.views
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+import android.util.Log
+import android.view.Window
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
-import com.isp.restaurantapp.R
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
 import com.isp.restaurantapp.databinding.ActivityMainBinding
-import com.isp.restaurantapp.viewModels.MainActivityVM
+import com.isp.restaurantapp.R
+import java.util.concurrent.Executors
 
+private const val CAMERA_PERMISSION_REQUEST_CODE = 1
+
+@ExperimentalGetImage
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.setContentView(R.layout.activity_main)
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val viewModel = ViewModelProvider(this)[MainActivityVM::class.java]
 
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = this
+        if (hasCameraPermission()) {
+            bindCameraUseCases()
+        } else {
+            requestPermission()
+        }
     }
+
+
+    // Permission checks
+    private fun hasCameraPermission() =
+        ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            bindCameraUseCases()
+        } else {
+            Toast.makeText(this, "Camera permission required", Toast.LENGTH_LONG).show()
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
+    // Usecases
+    private fun bindCameraUseCases() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val previewUseCase = Preview.Builder().build()
+                .also {
+                    it.setSurfaceProvider(binding.cameraView.surfaceProvider)
+                }
+
+            val options = BarcodeScannerOptions.Builder().setBarcodeFormats(
+                Barcode.FORMAT_QR_CODE
+            ).build()
+
+            val scanner = BarcodeScanning.getClient(options)
+
+            val analysisUseCase = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+
+            analysisUseCase.setAnalyzer(
+                Executors.newSingleThreadExecutor(),
+                { imageProxy -> processImageProxy(scanner, imageProxy, cameraProvider) }
+            )
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    previewUseCase,
+                    analysisUseCase
+                )
+            } catch (illegalStateException: IllegalStateException) {
+                // If the use case has already been bound to another lifecycle or method is not called on main thread.
+                Log.e("Exception", illegalStateException.message.orEmpty())
+            } catch (illegalArgumentException: IllegalArgumentException) {
+                // If the provided camera selector is unable to resolve a camera to be used for the given use cases.
+                Log.e("Exception", illegalArgumentException.message.orEmpty())
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+
+    // Analyze found qr code
+    private fun processImageProxy(barcodeScanner: BarcodeScanner, imageProxy: ImageProxy, cameraProvider: ProcessCameraProvider ) {
+        imageProxy.image?.let { image ->
+            val inputImage = InputImage.fromMediaImage(image, imageProxy.imageInfo.rotationDegrees)
+
+            barcodeScanner.process(inputImage).addOnSuccessListener { barcodeList ->
+                val barcode = barcodeList.getOrNull(0)
+
+//                barcode?.rawValue?.let { value ->
+////                        binding.bottomText.text =
+////                          getString(R.string.barcode_value, value)
+//
+//                        if (value == "Zkouska appky") {
+//                            //binding.bottomText.text = "Vítej u zkusebniho stolu"
+//                            val intent = Intent(this, secActivity::class.java).also {
+//                                it.putExtra("extraMsg", "Vítej u zkusebniho stolu")
+//                            }
+//                            startActivity(intent)
+//                        }
+//                }
+
+
+
+                for (bc in barcodeList) {
+                    val qrCode = bc.rawValue
+                    val correctQrCode = "SpravnyKod"
+                    if (qrCode == correctQrCode) {
+                        cameraProvider.unbindAll()
+
+                        val newAct = Intent(this, TableMainScreen::class.java)
+                            .also { it.putExtra("text", "Stul cislo 5") }
+                        startActivity(newAct)
+                    }
+
+
+
+                }
+
+
+
+            }
+                .addOnFailureListener {
+                    // Model could not be downloaded (bad connection, not signed in to google play...)
+                    Log.e("Exception", it.message.orEmpty())
+                }.addOnCompleteListener {
+                    imageProxy.image?.close()
+                    imageProxy.close()
+                }
+        }
+    }
+
+
+
+
+
 }

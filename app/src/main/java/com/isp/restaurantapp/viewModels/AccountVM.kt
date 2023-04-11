dcf487133bb115a19acbfede91757ecf82415c7b
pattysfall <dohnalpat@seznam.cz>
2023-04-11 17:58:18 +0200

    AccountFragment.kt and LoginFragment.kt now works and observes properly. Both uses view-models to observe their user.
    If a user is not yet logged in -> redirect to LoginFragment.kt,
    also if a user is already logged in -> redirect him to AccountFragment.kt

--- app/src/main/java/com/isp/restaurantapp/viewModels/AccountVM.kt
+++ app/src/main/java/com/isp/restaurantapp/viewModels/AccountVM.kt
@@ -1,7 +1,42 @@
 package com.isp.restaurantapp.viewModels
 
-import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.lifecycle.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class AccountVM : ViewModel() {
-    // TODO: Implement the ViewModel
+    companion object val TAG = "AccountVM"
+
+    // FIREBASE AUTH
+    private val _auth = FirebaseAuth.getInstance()
+
+    private val _userLogged: MutableLiveData<FirebaseUser> by lazy {
+        MutableLiveData<FirebaseUser>(_auth.currentUser)
+    }
+    val loggedUser: LiveData<String> = _userLogged.map {
+        // needs to be checked cause signout process nulls
+        // and ui still attempts to read/map it
+        if (it !=null)
+            it.email?: "Anonymous"
+        else ""// if its null, just map&read the empty string
+    }
+    val isUserLoggedIn: LiveData<Boolean> = _userLogged.map { (it != null) }
+
+    fun logOut() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response =_auth.signOut()
+                withContext(Dispatchers.Main){
+                    _userLogged.postValue(null)
+                }
+
+            } catch (e: Exception){
+                Log.e(TAG, "Error with user log in: ", e)
+            }
+        }
+    }
 }

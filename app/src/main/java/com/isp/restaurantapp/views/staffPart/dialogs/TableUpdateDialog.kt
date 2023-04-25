93bc602f15108153be33047e5004a1e729559ee7
pattysfall <dohnalpat@seznam.cz>
2023-04-25 15:56:30 +0200

    API URL is now encoded inside #*#url#*# sequence.
    Settings now works with preferences where is stored encoded url. URL is also set up inside StaffMainScreenVM.kt

--- app/src/main/java/com/isp/restaurantapp/views/staffPart/dialogs/TableUpdateDialog.kt
+++ app/src/main/java/com/isp/restaurantapp/views/staffPart/dialogs/TableUpdateDialog.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import com.isp.restaurantapp.databinding.StaffDialogUpdateTableBinding
-import com.isp.restaurantapp.models.dto.TableDTO
 import com.isp.restaurantapp.viewModels.StaffGoodsVM
 
 class TableUpdateDialog(): DialogFragment() {

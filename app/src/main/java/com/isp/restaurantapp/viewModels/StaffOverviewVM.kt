e709e8ed87d63a7e02aecc5ac97cba4aa895e73c
pattysfall <dohnalpat@seznam.cz>
2023-04-26 11:41:49 +0200

    Added deletePendingOrder to Overview

--- app/src/main/java/com/isp/restaurantapp/viewModels/StaffOverviewVM.kt
+++ app/src/main/java/com/isp/restaurantapp/viewModels/StaffOverviewVM.kt
@@ -1,13 +1,20 @@
 package com.isp.restaurantapp.viewModels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.Timestamp
+import com.isp.restaurantapp.models.Resource
 import com.isp.restaurantapp.models.dto.FrbOrderDTO
+import com.isp.restaurantapp.models.exceptions.OrderNotPendingDeleteException
+import com.isp.restaurantapp.models.firebase.FrbFieldsOrders
+import com.isp.restaurantapp.repositories.concrete.FrbOrderDeleter
 import com.isp.restaurantapp.repositories.concrete.FrbRealtimeOrderABC
+import com.isp.restaurantapp.repositories.interfaces.FrbDocumentDeleter
 import com.isp.restaurantapp.repositories.interfaces.FrbRealtimeGetterService
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -19,6 +26,21 @@ class StaffOverviewVM: ViewModel() {
     private val _allOrders = MutableLiveData<List<FrbOrderDTO>>()
     val allOrders: LiveData<List<FrbOrderDTO>>
         get() = _allOrders
+
+    private val _errorException: MutableLiveData<Throwable?> by lazy{
+        MutableLiveData<Throwable?>()
+    }
+    val errorException: LiveData<Throwable?>
+        get() = _errorException
+    fun resetErrorException(){
+        _errorException.postValue(null)
+    }
+
+    private val _orderDeleter: FrbDocumentDeleter<FrbOrderDTO> by lazy {
+        FrbOrderDeleter()
+    }
+
+
 
     fun getRealtimeOrdersAll(): LiveData<List<FrbOrderDTO>>{
         val itemsMLD = MutableLiveData<List<FrbOrderDTO>>()
@@ -32,6 +54,32 @@ class StaffOverviewVM: ViewModel() {
             }
         }
         return itemsMLD
+    }
+
+    fun deletePendingOrder(order: FrbOrderDTO){
+        val handler = CoroutineExceptionHandler{ _, throwable ->
+            if (throwable is OrderNotPendingDeleteException)
+                _errorException.postValue(throwable)
+            throwable.printStackTrace()
+        }
+        viewModelScope.launch(Dispatchers.IO + handler){
+            try {
+                if (order.state != FrbFieldsOrders.States.PENDING.value)
+                    throw OrderNotPendingDeleteException()
+                when (val result = _orderDeleter.deleteDocument(order)){
+                    is Resource.Failure -> throw result.exception
+                    is Resource.Success -> Log.i(
+                        PayVM.TAG,
+                        "deletePendingOrder: Order deleted successfully"
+                    )
+                    Resource.Loading -> {}
+                }
+            }
+            catch (e: Exception){
+                Log.e(PayVM.TAG, "deletePendingOrder: ${e.message}")
+                throw e
+            }
+        }
     }
 
     init {
